e5a8bb006eac20b011d98c97c925b2c7fbcb3830
David <i22safed@uco.es>
2017-03-12 18:11:04 +0100

    Finalizado ej1.c

--- p2/ej1.c
+++ p2/ej1.c
@@ -20,31 +20,31 @@ siguientes valores. { {1.1, 1.2, 1.3}, {2.1.,2.2, 2.3}}
 
 int main(){
 
-     float ** tabla[2][3]={{1.1,2.1,3.1},{1.2,2.2,2.3}};
+     float tabla[2][3]={{1.1,2.1,3.1},{1.2,2.2,2.3}};
 
      printf("\n¿Cual es el significado de tabla?\n");
      printf("Es una matriz bidimensional de 2x3 expresada con aritmetica de punteros\n\n");
 
      printf("¿Cual es el significado de (tabla+1)?\n");
-     printf(" \n\n");
+     printf(" %p\n\n", (tabla+1));
 
      printf("¿Cual es el significado de *(tabla+1)?\n");
-     printf(" \n\n");
+     printf(" %p\n\n",*(tabla+1));
 
      printf("¿Cual es el significado de (*(tabla+1)+1)?\n");
-     printf(" \n\n");
+     printf(" %p\n\n",(*(tabla+1)+1));
 
      printf("¿Cual es el significado de (*(tabla)+1)?\n");
-     printf(" \n\n");
+     printf(" %p\n\n",(*(tabla)+1));
 
      printf("¿Cual es el valor de *(*(tabla+1)+1)?\n");
-     printf(" \n\n");
+     printf("%f \n\n",*(*(tabla+1)+1));
 
      printf("¿Cual es el valor de *(*(tabla)+1)?\n");
-     printf(" \n\n");
+     printf(" %f\n\n",*(*(tabla)+1));
 
      printf("¿Cual es el valor de *(*(tabla+1))?\n");
-     printf("El valor de *(*(tabla+1)) es → %p\n\n", *(*(tabla+1)));
+     printf("El valor de *(*(tabla+1)) es → %f\n\n", *(*(tabla+1)));
 
 
 
