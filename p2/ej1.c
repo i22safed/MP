51bdce6e99dcb29c4eab9fe70dcc2ae541d93a61
David <i22safed@uco.es>
2017-03-15 20:17:33 +0100

    Finalizado el ej1.c

--- p2/ej1.c
+++ p2/ej1.c
@@ -26,25 +26,25 @@ int main(){
      printf("Es una matriz bidimensional de 2x3 expresada con aritmetica de punteros\n\n");
 
      printf("¿Cual es el significado de (tabla+1)?\n");
-     printf(" %p\n\n", (tabla+1));
+     printf("La dirección de comienzo de la tabla + 1 → %p\n\n", (tabla+1));
 
      printf("¿Cual es el significado de *(tabla+1)?\n");
-     printf(" %p\n\n",*(tabla+1));
+     printf("Es un puntero a la fila 1 → %p\n\n",*(tabla+1));
 
      printf("¿Cual es el significado de (*(tabla+1)+1)?\n");
-     printf(" %p\n\n",(*(tabla+1)+1));
+     printf("Puntero que apunta a la dirección de memoria tabla[1][1] → %p\n\n",(*(tabla+1)+1));
 
      printf("¿Cual es el significado de (*(tabla)+1)?\n");
-     printf(" %p\n\n",(*(tabla)+1));
+     printf("Apuntar a la fila 0 col 1 → %p\n\n",(*(tabla)+1));
 
      printf("¿Cual es el valor de *(*(tabla+1)+1)?\n");
-     printf("%f \n\n",*(*(tabla+1)+1));
+     printf("Contenido de la posición de tabla [1][1] → %f \n\n",*(*(tabla+1)+1));
 
      printf("¿Cual es el valor de *(*(tabla)+1)?\n");
-     printf(" %f\n\n",*(*(tabla)+1));
+     printf("Posición [0][1] → %f\n\n",*(*(tabla)+1));
 
      printf("¿Cual es el valor de *(*(tabla+1))?\n");
-     printf("El valor de *(*(tabla+1)) es → %f\n\n", *(*(tabla+1)));
+     printf("El valor de *(*(tabla+1)) es (pos [1][0]) → %f\n\n", *(*(tabla+1)));
 
 
 
