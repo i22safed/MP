693515e92b4a13690d09ded80a816b85aace45e2
David <i22safed@uco.es>
2017-03-15 20:32:23 +0100

    Comienzo de ej2.c y ej4.c

---
+++ p2/ej2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (){
+
+     int nEle = 0;
+
+
+
+     printf("Introduzca el numero de elementos del vector → ");
+     scanf("%i", &nEle);
+     
+
+
+
+     return 0;
+}
