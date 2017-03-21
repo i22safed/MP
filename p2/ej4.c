731b5da0a8c7a70b51c60dc7866ceb15e11775c9
David <i22safed@uco.es>
2017-03-21 20:47:35 +0100

    Finalizado ej4.c

--- p2/ej4.c
+++ p2/ej4.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 //PROTOTIPOS____________________________________________________
 
 int ** reservarMemoria(int nFil, int nCol);
+int * reservaVector(int nCol);
+void rellenaMatriz(int ** matriz, int nFil, int nCol);
+void imprimeMatriz(int ** matriz, int nFil, int nCol);
+int * minCol(int ** matriz, int nFil, int nCol, int * minimos);
+void imprimeVector(int * minimos, int nCol);
+void liberarMemoria(int *** matriz, int nFil);
 
 
 //MAIN__________________________________________________________
 
 int main (){
 
+     srand(time(NULL));
      int nFil=0 , nCol=0;
      int ** matriz=NULL;
+     int * minimos=NULL;
 
-     printf("Introduzca el numero de filas. ");
+
+     printf("Introduzca el numero de filas: ");
      scanf("%i",&nFil);
      printf("Introduzca el numero de columnas: ");
      scanf("%i",&nCol);
 
      matriz=reservarMemoria(nFil,nCol);
+     minimos=reservaVector(nCol);
 
+     rellenaMatriz(matriz,nFil,nCol);
 
+     printf("\n\nLa matriz es: \n");
+     imprimeMatriz(matriz,nFil,nCol);
+
+     minimos = minCol(matriz,nFil,nCol,minimos);
+
+     printf("El vector con los minimos de cada columna es → ");
+     imprimeVector(minimos,nCol);
+
+     liberarMemoria(&matriz,nFil);
 
      return 0;
 }
@@ -40,7 +61,109 @@ int ** reservarMemoria(int nFil, int nCol){
 
      }
 
-     printf("\nLa memoria ha sido reservada correctamente\n");
+     printf("\nLa memoria para la matriz de %ix%i ha sido reservada correctamente\n",nFil,nCol);
      return (M);
 
+}
+
+int * reservaVector(int nCol){					// No utilizamos referencia
+
+	int * v;
+
+	if((v=(int*)calloc(nCol,sizeof(int)))==NULL){
+
+		printf("\nError en la reserva de memoria\n");
+		exit(-1);
+
+	}else{
+
+		printf("\nLa reserva para %i elementos se ha realizado correctamente", nCol);
+	}
+
+	return (v);
+
+}
+
+void rellenaMatriz(int ** matriz, int nFil, int nCol){
+
+     int i=0, j=0;
+     int nAle=0;
+
+
+     for(i=0;i<nFil;i++){
+
+
+          for(j=0;j<nCol;j++){
+
+               nAle = rand () % (20-1+1) + 1;
+               *(*(matriz+i)+j)=nAle;
+
+          }
+     }
+}
+
+void imprimeMatriz(int ** matriz, int nFil, int nCol){
+
+     int i=0,j=0;
+
+     for(i=0;i<nFil;i++){
+
+          for(j=0;j<nCol;j++){
+
+
+               printf("%i ",*(*(matriz+i)+j));
+
+          }
+
+          printf("\n");
+     }
+
+}
+
+int * minCol(int ** matriz, int nFil, int nCol, int * minimos){
+
+     int i=0, j=0;
+     int minimo=20;
+
+     for(j=0;j<nCol;j++){
+
+          for(i=0;i<nFil;i++){
+
+               if((*(*(matriz+i)+j))<=minimo){
+
+                    minimo = (*(*(matriz+i)+j));
+
+               }
+          }
+
+          *(minimos+j)=minimo;
+          minimo = 20;
+
+     }
+
+     return (minimos);
+}
+
+void imprimeVector(int * minimos, int nCol){
+
+
+     int i=0;
+
+     for(i=0;i<nCol;i++){
+
+          printf("%i ", *(minimos+i));
+
+     }
+
+     printf("\n");
+
+}
+
+void liberarMemoria(int *** matriz, int nFil){
+
+     int i=0;
+
+     for(i=0;i<nFil;i++){
+          free(*(matriz+i));
+     }
 }
