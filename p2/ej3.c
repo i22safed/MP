c93102fcaa2a35d969817d40b780c227a2367e68
David <i22safed@uco.es>
2017-03-23 17:39:40 +0100

    Funcion de reserva de memoria realizada

--- p2/ej3.c
+++ p2/ej3.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Ficha_Jugador{
 
 	char nombre[50];
-	int dorsal; 
-	float peso; 
-	int estatura; 
-	
+	int dorsal;
+	float peso;
+	int estatura;
+
 };
-struct Ficha_Jugador * reservaVector(struct Ficha_Jugador * jugador, int nEle);
+struct Ficha_Jugador * reservaVector(int nEle);
 
 
 int main (){
 
-	int nEle=0; 
-	struct Ficha_Jugador * jugadores; 
+	int nEle=0;
+	struct Ficha_Jugador * jugador;
+	struct Ficha_Jugador aux;
 
 	printf("\nIntroduzca el numero de jugadores para la reserva: ");
 	scanf("%i",&nEle);
 
 
+	jugador = reservaVector(nEle);
 
 
 
 
-
-
-
-	return 0; 
+	return 0;
 }
 
-struct Ficha_Jugador * reservaVector(struct Ficha_Jugador * jugador, int nEle){
+struct Ficha_Jugador * reservaVector(int nEle){
 
-	struct Ficha_Jugador * p; 
+	struct Ficha_Jugador * p;
 
-	if((p=(struct Ficha_Jugador *) calloc(nEle, sizeof(strutc Ficha_Jugador)))==NULL){
+	if((p=(struct Ficha_Jugador *) malloc(nEle*sizeof(struct Ficha_Jugador)))==NULL){
 
 
 		printf("\nFallo en la reserva de memoria\n");
@@ -43,7 +43,7 @@ struct Ficha_Jugador * reservaVector(struct Ficha_Jugador * jugador, int nEle){
 
 	}else{
 
-		printf("\nLa reserva de memoria se ha realizado correctamente\n");
+		printf("\nLa reserva de memoria para %i elementos se ha realizado correctamente\n",nEle);
 
 	}
 }
