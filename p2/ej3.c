dc8df0a0d4680c1c47da5746c5e31c354f7f345d
David <i22safed@uco.es>
2017-03-24 19:53:58 +0100

    Realizado el listado de jugadores

--- p2/ej3.c
+++ p2/ej3.c
@@ -11,21 +11,21 @@ struct Ficha_Jugador{
 
 };
 struct Ficha_Jugador * reservaVector(int nEle);
+struct Ficha_Jugador leerJugador();
+struct Ficha_Jugador * rellenaVectorJugadores(struct Ficha_Jugador * jugador, int nEle);
 
 
 int main (){
 
-	int nEle=0;
+	int nEle=0, i=0;
 	struct Ficha_Jugador * jugador;
-	struct Ficha_Jugador aux;
 
 	printf("\nIntroduzca el numero de jugadores para la reserva: ");
 	scanf("%i",&nEle);
-
+	getchar(); // Necesario para que no se salte scanf's en funciones
 
 	jugador = reservaVector(nEle);
-
-
+	jugador = rellenaVectorJugadores(jugador,nEle);
 
 
 	return 0;
@@ -44,6 +44,59 @@ struct Ficha_Jugador * reservaVector(int nEle){
 	}else{
 
 		printf("\nLa reserva de memoria para %i elementos se ha realizado correctamente\n",nEle);
+		return (p);
 
 	}
+}
+
+struct Ficha_Jugador leerJugador(){
+
+	struct Ficha_Jugador aux;
+
+	printf("\nIntroduzca el nombre del jugador → ");
+	gets(aux.nombre);
+
+	printf("\nIntroduzca el dorsal del jugador → ");
+	scanf("%i", &aux.dorsal);
+
+	printf("\nIntroduzca el peso del jugador → ");
+	scanf("%f",&aux.peso);
+
+	printf("\nIntroduzca la estatura del jugador → ");
+	scanf("%i",&aux.estatura);
+
+
+	return aux;
+
+}
+
+struct Ficha_Jugador * rellenaVectorJugadores(struct Ficha_Jugador * jugador, int nEle){
+
+	int i=0;
+
+	for(i=0;i<nEle;i++){
+
+		printf("\nDatos para el jugador %i ______________________", i);
+
+		*(jugador+i)=leerJugador();
+		getchar();
+
+	}
+
+	return jugador;
+
+}
+
+void listarJugadores(struct Ficha_Jugador * jugador, int nEle){
+
+	int i=0;
+
+	for(i=0;i<nEle;i++){
+
+		printf("\nDatos del jugador %i",i);
+		printf("Nombre → %s",(jugador+i)->nombre);
+
+	}
+
+
 }
