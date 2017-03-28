cf2147b6e2921ea02a1a260df815e4e744279ff5
David <i22safed@uco.es>
2017-03-28 20:33:44 +0200

    Comienzo del ej5.c

---
+++ p2/ej5.c
@@ -0,0 +1,16 @@
+
+/*
+
+5.  Escribe un programa que lea una frase y, a partir de ella, cree un vector
+dinámico de cadenas con las diferentes palabras de la frase. A partir de este
+vector, el programa deberá:
+
+     · Calcular la longitud media de las palabras de la frase, así como la
+     longitud mayor y menor (usando paso de parámetros por referencia).
+     · Construir un vector dinámico con la frecuencia de aparición de cada
+     longitud.
+
+Realiza una adecuada modularización, implementando todas las funciones que sean
+necesarias.
+
+*/
