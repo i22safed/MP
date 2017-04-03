de33c5d0518d256519cf6d4c11725e817c511524
David <i22safed@uco.es>
2017-04-03 12:46:25 +0200

    Finalizado ej3.c

---
+++ p3/ej3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int divisionRecursiva(int dividendo,int divisor, int cociente, int resto);
+
+
+int main(){
+
+
+     int dividendo=0,divisor=0,cociente=0,resto=0;
+
+     printf("\nIntroduzca el dividendo → ");
+     scanf("%i",&dividendo);
+     printf("\nIntroduzca el divisor → ");
+     scanf("%i",&divisor);
+
+     cociente = divisionRecursiva(dividendo,divisor,cociente,resto);
+
+     printf("\nEl resultado de la división de %i entre %i es → %i\n",  dividendo,divisor,cociente);
+
+     return 0;
+
+}
+
+
+int divisionRecursiva(int dividendo,int divisor, int cociente, int resto){
+
+     if(dividendo < divisor){
+
+          return cociente;
+
+     }else{
+
+          cociente++;
+          dividendo = divisionRecursiva((dividendo - divisor),divisor,cociente,resto);
+
+     }
+
+}
