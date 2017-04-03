2e8cafc0bc7f264b9df187f08efcc787d931ba12
David <i22safed@uco.es>
2017-04-03 09:06:19 +0200

    Finalizado ej7.c

--- p2/ej7.c
+++ p2/ej7.c
@@ -22,25 +22,32 @@ int main(){
      scanf("%i",&nColB);
 
 
-     if(nColA!=nFilA){
+     if(nColA!=nFilB){
 
           printf("\nEl numero de columnas de A es distinto que el numero de filas de B");
           exit (-1);
      }
 
-     matriz1 = reservarMemoria(nFil,nCol);
-     matriz2 = reservarMemoria(nFil,nCol);
+     matrizA = reservarMemoria(nFilA,nColA);
+     matrizB = reservarMemoria(nFilB,nColB);
 
-     rellenaMatriz(int ** matrizA, int nFilA, int nColA);
-     rellenaMatriz(int ** matrizB, int nFilB, int nColB);
+     rellenaMatriz( matrizA, nFilA, nColA);
+     rellenaMatriz( matrizB, nFilB, nColB);
 
-     printf("\nLa matriz A es : ");
-     imprimeMatriz(int ** matrizA, int nFilA, int nColA);
+     printf("\nLa matriz A es : \n");
+     imprimeMatriz(matrizA,nFilA,nColA);
 
-     printf("\nLa matriz B es : ");
-     imprimeMatriz(int ** matrizB, int nFilB, int nColB);
+     printf("\nLa matriz B es : \n");
+     imprimeMatriz(matrizB,nFilB,nColB);
 
      matrizResultado = multiplicaMatriz( matrizA, matrizB, nFilA, nColA, nFilB, nColB);
+
+     printf("\nLa matriz resultante es: \n");
+     imprimeMatriz(matrizResultado,nFilA,nColB);
+
+     liberarMemoria(matrizA,nFilA);
+     liberarMemoria(matrizB,nFilB);
+
 
      return 0;
 
@@ -48,22 +55,61 @@ int main(){
 
 int ** multiplicaMatriz(int ** matrizA, int ** matrizB, int nFilA, int nColA, int nFilB, int nColB){
 
-     int i=0,j=0;
+     int i=0,j=0,k=0;
      int res=0;
 
      int ** matrizRes;
-     reservarMemoria(nFilA,nColB);
+     matrizRes = reservarMemoria(nFilA,nColB);
 
-     for(i=0;i<nFilA;i++){
 
-          for(j=0;j<nColA;j++){
-
-               res = res + matrizA[i][j]*matriz[j][i];
-
-          }
-
-     }
+       for(i=0;i<nFilA;i++){
+           for(j=0;j<nColB;j++){
+               matrizRes[i][j]=0;
+               for(k=0;k<nColA;k++){
+                   matrizRes[i][j]=(matrizRes[i][j]+(matrizA[i][k]*matrizB[k][j]));
+               }
+           }
+       }
 
      return matrizRes;
 
 }
+
+/*
+
+     Matriz A       Matriz B       MatrizRes
+
+     2x3            3x2            2x2
+
+
+     Iteraciones → nFilA x nColA
+
+     1ra Iteracion       [0][0] * [0][0]         2ra Iteracion       [0][0] * [0][1]
+                         [0][1] * [1][0]                             [0][1] * [1][1]
+                         [0][2] * [2][0]                             [0][2] * [2][1]
+
+     fila 0 x columna 0 → pos [0][0]              fila 0 columna 1 → pos [0][1]
+
+
+     3ra Iteracion       [1][0] * [0][0]         4ra Iteracion       [1][0] * [0][1]
+                         [1][1] * [1][0]                             [1][1] * [1][1]
+                         [1][2] * [2][0]                             [1][2] * [2][1]
+
+     fila 1 x columna 0 → pos [1][0]              fila 1 columna 1 → pos [1][1]
+
+
+     · Algoritmo
+
+          for(i=0;i<nFilA;i++){
+              for(j=0;j<nColB;j++){
+                  matrizRes[i][j]=0;
+                  for(k=0;k<nColA;k++){
+                      matrizRes[i][j]=(matrizRes[i][j]+(matrizA[i][k]*matrizB[k][j]));
+                  }
+              }
+          }
+
+
+
+
+*/
