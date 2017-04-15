3a6e4860ce4577e5ccba683210890d62e7ce95ea
David <i22safed@uco.es>
2017-04-15 19:33:06 +0200

    Comienzo de ej9.c

---
+++ p3/ej9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct stock {
+
+     char titulo[20];
+     char autor[20];
+     float precio = 0;
+     int unidades = 0;
+
+};
+
+
+
+int main(int argc, char ** argv){
+
+     struct stock libro;
+
+
+
+
+
+
+
+}
